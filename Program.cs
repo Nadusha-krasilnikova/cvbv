8ace5d514566c04269ec6f13127f523bd4a91304
Nadusha <nadusha230275@gmail.com>
2022-06-28 17:51:58 +0500

    000007

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// приветствие по особенному 
+Console.Write("Введите имя ");
+String username=Console.ReadLine();
+if (username.ToLower()=="надя")
+{
+    Console.WriteLine("Пипец, это же Надя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
